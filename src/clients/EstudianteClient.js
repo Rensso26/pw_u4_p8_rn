d37b5114cc9072ffc0b45f358bb0978cd82eec41
Rensso26 <rnparra@uce.edu.ec>
2025-07-16 17:40:47 -0500

    Taller 36

---
+++ src/clients/EstudianteClient.js
@@ -0,0 +1,49 @@
+import axios from "axios";
+
+
+const URL_API = "http://localhost:8081/api/matricula/v1/estudiantes";
+//Guardar
+//public void guardar(@RequestBody Estudiante estudanteTo)
+
+const guardar = async (body) => {
+    const data = axios.post(URL_API, body).then(r => r.data);
+    console.log(data);
+}
+
+
+//public Response actualizar(@RequestBody EstudianteTo estudianteTo, @PathParam("id") Integer id){
+const actualizar = async (body, id) => {
+    const data = axios.put(`${URL_API}/${id}`,body).then(r => r.data);
+    console.log(data);
+}
+
+//public Response actualizarParcialPorId(@RequestBody EstudianteTo estudianteTo, @PathParam("id") Integer id, @Context UriInfo uriInfo){
+
+const actualizarParcial = async (body, id) => {
+    const data = axios.patch(`${URL_API}/${id}`,body).then(r => r.data);
+    console.log(data);
+}
+
+//    public Response borrarPorId(@PathParam("id") Integer id){
+const borrarPorId = async (id)=>{
+    axios.delete(`${URL_API}/${id}`).then(r => r.data);
+
+}
+
+
+
+//fachadas
+export const guardarFachada = async (body) => {
+    await guardar(body);
+}
+
+export const actualizarFachada = async (body,id) => {
+    await actualizar(body,id);
+}
+
+export const actualizarParcialFachada = async (body, id) => {
+    await actualizarParcial(body,id);
+}
+export const borrarPorIdFachada = async (id)=>{
+    await borrarPorId(id);
+}
---
+++ src/components/Estudiante.vue
@@ -0,0 +1,49 @@
+<template>
+<div>
+    <button v-on:click="guardar()">Guardar</button>
+    <button v-on:click="actualizar()">Actualizar</button>
+    <button v-on:click="actualizarParcial()">Actualizar Parcial</button>
+    <button v-on:click="borrar()">Borrar</button>
+</div>
+</template>
+
+<script>
+import {guardarFachada, actualizarFachada, actualizarParcialFachada,borrarPorIdFachada} from "../clients/EstudianteClient.js"
+export default {
+    methods:{
+       async guardar(){
+            var fecha='2000-12-26';
+            const estudianteToBody = {
+                nombre: "Rensso",
+                apellido: "Parra",
+                fechaNacimiento: fecha + "T00:00:00",
+                genero: "M",
+            };
+            await guardarFachada(estudianteToBody);
+        },
+        async actualizar(){
+            let fecha = '2000-12-26';
+            const estudianteToBody = {
+                nombre: "Rensso",
+                apellido: "Parra",
+                fechaNacimiento: fecha + "T00:00:00",
+                genero: "M",
+            };
+            await actualizarFachada(estudianteToBody,1);
+        },
+        async actualizarParcial(){
+            const estudianteToBody = {
+                apellido: "Vasquez",
+            };
+            await actualizarParcialFachada(estudianteToBody,1);
+        },
+        async borrar(){
+            await borrarPorIdFachada(2);
+        },
+    },
+};
+</script>
+
+<style>
+
+</style>
